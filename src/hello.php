c4b0597471ea0487f05e8cf58ff6ef73508bb037
David Ang <davidang09@gmail.com>
2017-10-08 21:37:35 +0800

    example code that sets and retrieves values from a memcached server

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,22 @@
 <?php
-  echo "I'm being processed by PHP-FPM inside a Docker container.";
+$cache = new Memcached();
+$cache->addServer(getenv('MEMCACHED_PORT_11211_TCP_ADDR'), getenv('MEMCACHED_PORT_11211_TCP_PORT'));
+
+$seconds = $_GET['s'] ?? 5;
+$cacheKey = $_SERVER['QUERY_STRING'];
+
+if($cacheKey) {
+  if($cache->get($cacheKey)) {
+    echo $cache->get($cacheKey);
+  } else {
+    $cache->set($cacheKey, "$seconds has been retrieved from the cache.");
+
+    # pretend we have a long running task
+    sleep($seconds);
+
+    echo "$seconds has been stored in the cache.";
+  }
+} else {
+  sleep($seconds);
+}
 ?>
